227533f6f11eca85cbe92b5159ed7b9312e429b1
AprendetEsto <40726945+AprendetEsto@users.noreply.github.com>
2018-09-01 16:37:10 -0500

    Adición de publicaciones
    Se adicionaron archivos relacionados a las publicaciones de LecturaXML y Validación de archivos XML usando XSD

---
+++ LecturaXML/Program.cs
@@ -0,0 +1,67 @@
+﻿/* 
+   * Proyecto: LecturaXML
+   * Descripción: Este proyecto se realizo con la finalidad de ejemplificar lo mencionado
+   * en la publicación "Lectura de archivos xml - XmlDocument"
+   * Fecha: 2018-06-30 
+   * UrlPublicacion: https://aprendetesto.blogspot.com/2018/07/lectura-de-archivos-xml-xmldocument.html
+*/
+
+using System;
+using System.Xml;
+
+namespace LecturaXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Presiona una tecla para comenzar...");
+            Console.ReadLine();
+
+            string sXml = "<Usuario nombre = \"Wade\" apellido = \"El deforme Wilson\">" +
+                            "<Perfil fechaRegistro = \"2018-06-30T17:31:02\" status = \"activo\"></Perfil>" +
+                                "<Preferencias status = \"true\">" +
+                                    "<Preferencia nombre = \"Programacion\" valor = \"C#, ASP.NET, MVC, Kotlin, MongoDB, WebService\"></Preferencia>" +
+                                    "<Preferencia nombre = \"Cocina\" valor = \"Recetas a la parrilla, mariscos, comida picante\"></Preferencia>" +
+                                    "<Preferencia nombre = \"Gatos\" valor = \"Gorditos y bonitos, miau!\" ></Preferencia>" +
+                                "</Preferencias>" +
+                          "</Usuario>";
+
+            try
+            {
+                XmlDocument oXmlDoc = new XmlDocument();
+                oXmlDoc.LoadXml(sXml);
+
+                /* - Obtener el valor de Usuario.apellido. */
+                string sApellido = oXmlDoc.DocumentElement.GetAttribute("apellido");
+
+                /* - Obtener el valor de Perfil.fechaRegistro. */
+                XmlElement oPerfil = ((XmlElement)oXmlDoc.DocumentElement.GetElementsByTagName("Perfil")[0]);
+                string sFechaRegistro = oPerfil.GetAttribute("fechaRegistro");
+
+                /* - Obtener el valor de Preferencias:Preferencia.valor donde el atributo nombre tenga el valor Gatos. */
+                XmlNodeList oPreferencia = oXmlDoc.DocumentElement.GetElementsByTagName("Preferencia");
+                string sValor = string.Empty;
+
+                foreach (XmlElement oNodo in oPreferencia)
+                {
+                    if (oNodo.GetAttribute("nombre") == "Gatos")
+                        sValor = oNodo.GetAttribute("valor");
+                }
+
+                /* Mostrar salidas */
+                Console.WriteLine(string.Format("Punto 1: {0}", sApellido));
+                Console.WriteLine(string.Format("Punto 2: {0}", sFechaRegistro));
+                Console.WriteLine(string.Format("Punto 3: {0}", sValor));
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+            Console.WriteLine("\r\nPresiona una tecla para terminar...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ValidacionXSD/Program.cs
@@ -0,0 +1,107 @@
+﻿/* 
+   * Proyecto: Validación de XML utilizando su respectivo XSD
+   * Descripción: Valida XML correspondientes a un CFDI v3.3 (facturación SAT, México)
+   * Fecha: 2018-08-19
+   * UrlPublicacion: 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.IO;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace ValidacionXSD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Presiona una tecla para comenzar...");
+            Console.ReadLine();
+
+            try
+            {
+                /* Lectura de cfdi */
+                string sPathXML = @".\xml\cfdi_1.xml";
+                byte[] yXml = File.ReadAllBytes(sPathXML);
+
+                /* Llamado al método y verificación de resultado */
+                string sResultado = Valida_con_XSD(yXml);
+                if (string.IsNullOrWhiteSpace(sResultado))
+                {
+                    Console.WriteLine("Xml válido.");
+                    Console.ReadLine();
+                }
+
+                Console.WriteLine("Xml inválido: " + sResultado);
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+            Console.WriteLine("Presiona una tecla para terminar...");
+            Console.ReadLine();
+        }
+
+
+        /// <summary>
+        /// Método de validación XML contra XSD
+        /// </summary>
+        /// <param name="yXML">Secuencia de bytes del XML</param>
+        /// <returns>string. Empty si el resultado es válido de lo contrario devuleve un mensaje con información del error</returns>
+        private static string Valida_con_XSD(byte[] yXML)
+        {
+            MemoryStream oMemoStream = null;
+            XmlReader oXmlReader = null;
+            XmlReaderSettings oXmlReadSettings = null;
+
+            try
+            {
+                /* Configuración del validador */
+                oXmlReadSettings = new XmlReaderSettings();
+                oXmlReadSettings.ValidationType = ValidationType.Schema;
+
+                /* Colección de archivos XSD */
+                XmlSchemaSet oSchemaSet = new XmlSchemaSet();
+                oSchemaSet.XmlResolver = null;
+                oSchemaSet.Add(null, @".\xsd\cfdv33.xsd");
+                oSchemaSet.Add(null, @".\xsd\catCFDI.xsd");
+                oSchemaSet.Add(null, @".\xsd\tdCFDI.xsd");
+                oSchemaSet.Compile();
+
+                /* Asignación de shcemas */
+                oXmlReadSettings.Schemas = oSchemaSet;                
+
+                /* Lectura y validación del CFDI*/
+                oMemoStream = new MemoryStream(yXML);
+                oXmlReader = XmlReader.Create(oMemoStream, oXmlReadSettings);
+                while (oXmlReader.Read()) { }
+
+                return string.Empty;
+            }
+            catch(XmlSchemaException XmlSchemaEx)
+            {
+                /* Captura de mensajes */
+                return XmlSchemaEx.Message;
+            }
+            catch(XmlException XmlEx)
+            {
+                /* Captura de mensajes */
+                return XmlEx.Message;
+            }
+            catch (Exception ex)
+            {
+                /* Algo salio muy mal 7n7 */
+                throw;
+            }
+        }
+
+        
+    }
+}
